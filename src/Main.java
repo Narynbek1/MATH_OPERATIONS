170027e34b755d182f4d6c9b975704203c9d1853
naryn <narynbeknurbekov@gmail.com>
2023-06-07 20:47:17 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//a = a + 5; increment bul bir sandyn 1 koboiuusu a++;
+        //a = a + 1;
+
+//        int a = 3;
+//        a++;
+//        a *= 133;
+//        System.out.println(a);
+
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int house = a * 60 * 60;
+        int minute = b * 60;
+        int sum = house + minute + c;
+        System.out.println(sum);
+
+    }
+}
